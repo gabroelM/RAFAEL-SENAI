f0a864d8f5b118f1282dd2b9869955d9973c538a
gabroelM <166452516+gabroelM@users.noreply.github.com>
2024-04-15 21:58:09 -0300

    Add files via upload

---
+++ Aula 6/codigos.js
@@ -0,0 +1,34 @@
+// let quadrado = document.getElementById("divquadrado")
+// let cont = 1
+// let dobro = 0
+// while (cont <= 10){
+//     dobro = cont ** 2
+//     quadrado.innerHTML += `O quadrado de ${cont} é ${dobro}<br>`
+//     cont++
+// }
+
+// let decrescente = document.getElementById("divdescrecente")
+// let conta = 10
+// while (cont >= 1){
+    
+//    decrescente.innerHTML += `${conta}`
+//     cont--
+// }
+ 
+// let soma = document.getElementById("divsoma")
+// let contad = 1
+// let acumalador = 0
+// while (contad <= 100){
+//     acumalador = acumalador + contad
+//     contad++
+// }
+//     soma.innerHTML = `O seu total foi${acumalador}`
+
+
+// let tabuada5 = document.getElementById("divtabuada5")
+// let tabu = 5
+// let contado = 1
+// while (contado <= 10){
+//     tabuada5.innerHTML += `${tabu}X${contado}=${tabu*contado}<br>`
+//     contado++
+// }
