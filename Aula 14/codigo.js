2bac6adf7fcf656e734511143391e840119909ea
gabroelM <166452516+gabroelM@users.noreply.github.com>
2024-05-15 22:04:23 -0300

    Add files via upload

---
+++ Aula 14/codigo.js
@@ -0,0 +1,34 @@
+/*let impares = []
+let soma = 0
+
+for(let contagem=0;contagem < 5;contagem++){
+    let numero = Number(prompt("Digite um numero inteiro"))
+    if(numero % 2 != 0){
+
+    impares.push(numero)
+    soma += numero
+    }
+}
+
+alert(`A soma dos numeros impares é ${soma}`)*/
+/*let soma = 0
+let menor = 11
+let maior = 0
+let notas = []
+for(let i = 0;i < 6;i++){
+    let nota = Number(prompt("Digite um nota"))
+    soma += nota
+    if(nota > maior){
+        menor = nota
+    }
+    if(nota < menor){
+        menor = nota
+    }
+}
+soma = soma - (maior + menor)
+console.log(notas)
+console.log(menor)
+console.log(maior)
+let media = soma / 4
+console.log(media)*/
+
