c03a43aaeb5a257fc4946827d48ecbab470b4347
gabroelM <166452516+gabroelM@users.noreply.github.com>
2024-06-25 21:48:00 -0300

    Add files via upload

---
+++ Projeto Final/codigo.js
@@ -0,0 +1,9 @@
+let array = []
+function addProta(evento){
+evento.preventDefault()
+const nome = document.getElementById(`nome`).value
+const ator = document.getElementById(`ator`).value
+const foto = document.getElementById(`foto`).value
+const skill = document.getElementById(`skill`).value
+const descri = document.getElementById(`descri`)
+}
